826d37bfeb7a9d662fa23c1dc0e5c928f9261d76
Gabe Kahen <gabe.kahen@onvoy.com>
2017-03-27 15:41:00 -0600

    Fixed formatting issues, added boombox case statement

--- boombox.go
+++ boombox.go
@@ -7,6 +7,11 @@ import (
   "github.com/inteliquent/casatunes"
   "github.com/nlopes/slack"
 )
+
+var (
+  boombox_started bool = false
+)
+
 func casa_BoomBox(slack_api *slack.Client, ev *slack.MessageEvent) {
   //CASA_ENDPOINT := os.Getenv("CASA_ENDPOINT")
   //casa_api := casatunes.New(CASA_ENDPOINT)
@@ -14,7 +19,7 @@ func casa_BoomBox(slack_api *slack.Client, ev *slack.MessageEvent) {
   message_parameters := slack.NewPostMessageParameters()
   message_parameters.AsUser = true
 
-  userInput := regexp_boombox.FindStringSubmatch(ev.Text)[1]
+  user_input := regexp_boombox.FindStringSubmatch(ev.Text)[1]
 
   checkInterval, err := time.ParseDuration("2s")
 
@@ -22,42 +27,41 @@ func casa_BoomBox(slack_api *slack.Client, ev *slack.MessageEvent) {
     log.Fatal(err)
   }
 
-  switch userInput {
+  switch user_input {
   case "start":
-    log.Println("Starting BoomBox")
-    bbTicker := time.NewTicker(checkInterval)
-    bbQuit := make(chan int, 1)
-    go boomBox(bbTicker.C, bbQuit, slack_api, ev)
+    if !boombox_started {
+      log.Println("Starting BoomBox")
+      bb_ticker := time.NewTicker(checkInterval)
+      bb_quit := make(chan int, 1)
+      go boomBox(bb_ticker.C, bb_quit, slack_api, ev)
+      boombox_started = true
+    }
   case "stop":
+    if boombox_started {
+
+    }
   }
-  /*
-  slack_api.PostMessage(
-    ev.Channel,
-    "",
-    message_parameters,
-  )
-*/
 }
 
 func boomBox(tick <-chan time.Time, quit chan int, slack_api *slack.Client, ev *slack.MessageEvent) {
   CASA_ENDPOINT := os.Getenv("CASA_ENDPOINT")
   casa_api := casatunes.New(CASA_ENDPOINT)
 
-  npCurrent, err := casa_api.NowPlaying("0")
+  np_current, err := casa_api.NowPlaying("0")
   if err != nil {
     log.Fatal(err)
   }
-  npCheck := &casatunes.RESTNowPlayingMediaItem{}
+  np_check := &casatunes.RESTNowPlayingMediaItem{}
   for {
     select {
     case <-tick:
-      npCheck, err = casa_api.NowPlaying("0")
+      np_check, err = casa_api.NowPlaying("0")
       if err != nil {
         log.Fatal(err)
       }
-      if npCheck.CurrSong.ID != npCurrent.CurrSong.ID {
+      if np_check.CurrSong.ID != np_current.CurrSong.ID {
         casa_NowPlaying(slack_api, ev)
-        npCurrent = npCheck
+        np_current = np_check
       }
     case <-quit:
       log.Printf("Stopping boombox in channel %s", ev.Channel)
